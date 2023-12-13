9880b8cf85d79dae7c58cd5a02dbdc83336176a0
Owen <jeffowen04@gmail.com>
2023-12-13 13:01:11 +0300

    postgres setup

--- excel3/settings.py
+++ excel3/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +84,17 @@ WSGI_APPLICATION = 'excel3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+'''
+
+DATABASES = {
+    "default":dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
 
 
